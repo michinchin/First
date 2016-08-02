c60880100aa92e916f7006aeb3c108c434a6245f
Abigail Chin <chinabigail08@gmail.com>
2016-08-02 15:17:00 -0700

    Initial Commit

---
+++ newTEst/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  newTEst
+//
+//  Created by Abigail Chin on 8/2/16.
+//  Copyright © 2016 Abigail Chin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
