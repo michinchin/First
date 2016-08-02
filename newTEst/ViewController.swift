d149144d5dc90c7f0ee135dcae6945a82fb7669c
Abigail Chin <chinabigail08@gmail.com>
2016-08-02 16:06:37 -0700

    stopwatch label changes with button pushes

--- newTEst/ViewController.swift
+++ newTEst/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBAction func startTime(sender: AnyObject) {
+        timeLabel.text = "Start!"
+    }
+    @IBAction func stopTime(sender: AnyObject) {
+        timeLabel.text = "Stop!"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
